29d3cf24aeb71449f4a9183e58b615c6b40405df
Sam <sammy.unterman@gmail.com>
2016-06-04 21:23:34 -0400

    adding workboss and sum of digits

--- sum_of_digits.rb
+++ sum_of_digits.rb
@@ -1,6 +1,16 @@
 digit_arr = []
-File.open('sum_of_digits.txt').each do |i|
-  digit_arr << i
-end
+split_arr = []
 
+File.open('sum_of_digits.txt').each do |first_loader|
+  digit_arr << first_loader #load the array with the numbers on each line representing 1 index
+end
 puts digit_arr
+
+
+for i in 0..digit_arr.length
+  digit_arr[i].each_char { |x| p x}
+end
+  # split_count.to_s.each_char { |c| split_arr << split_count, '' }
+  # split_arr << split_count
+  # puts split_count
+# puts split_arr
