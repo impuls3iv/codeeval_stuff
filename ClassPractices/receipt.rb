f8c8dfe8ea3395f35a02b655edd698c5de49a29f
Sam <sammy.unterman@gmail.com>
2016-06-12 15:17:24 -0400

    wrote first version of receipt.rb

--- ClassPractices/receipt.rb
+++ ClassPractices/receipt.rb
@@ -6,7 +6,24 @@ class Receipt
 
     do until answer == 'none'
 
-      
+      puts "What is the name of the product?"
+      @name_r = gets.chomp!
+
+      puts "What is the products category? food, medicing (med) and books are tax exempt"
+      @cat_r = gets.chomp!
+
+      puts "Is the product imported?"
+      @imp_r = gets.chomp!
+
+      puts "What is the cost of the product?"
+      @cost_r = gets.chomp!
+
+      puts "What is the code of the product?"
+      @instance_name = gets.chomp!
+
+      @instance_name = Item.new(@name_r, @cat_r, @imp_r, @cost_r)
+
+
 
     end
 
