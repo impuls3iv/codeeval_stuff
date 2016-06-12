049dac1dd863c1ed5a9fde0ba497dbfcf489d642
Sam <sammy.unterman@gmail.com>
2016-06-12 15:44:03 -0400

    removed loop from receipt.rb

--- ClassPractices/receipt.rb
+++ ClassPractices/receipt.rb
@@ -2,30 +2,26 @@ require_relative 'item'
 
 class Receipt
 
-  def sales
+  def add_products
 
-    do until answer == 'none'
 
-      puts "What is the name of the product?"
+      puts "What is the name of the product? (X to quit)"
       @name_r = gets.chomp!
 
-      puts "What is the products category? food, medicing (med) and books are tax exempt"
+      puts "What is the products category? food, medicing (med) and books are tax exempt (X to quit)"
       @cat_r = gets.chomp!
 
-      puts "Is the product imported?"
+      puts "Is the product imported? (X to quit)"
       @imp_r = gets.chomp!
 
-      puts "What is the cost of the product?"
+      puts "What is the cost of the product? (X to quit)"
       @cost_r = gets.chomp!
 
-      puts "What is the code of the product?"
+      puts "What is the code of the product? (X to quit)"
       @instance_name = gets.chomp!
 
       @instance_name = Item.new(@name_r, @cat_r, @imp_r, @cost_r)
 
-
-
-    end
 
   end
 
